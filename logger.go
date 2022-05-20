7e7b6e47f46fef4d33433758fb2d9a6b0772d277
wagslane <lane.c.wagner@gmail.com>
2022-05-20 09:57:00 -0600

    log levels

--- logger.go
+++ logger.go
@@ -22,23 +22,23 @@ const loggingPrefix = "gorabbit"
 type stdDebugLogger struct{}
 
 func (l stdDebugLogger) FatalF(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("%s FATAL: %s", loggingPrefix, format), v...)
 }
 
 func (l stdDebugLogger) ErrorF(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("%s ERROR: %s", loggingPrefix, format), v...)
 }
 
 func (l stdDebugLogger) WarnF(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("%s WARN: %s", loggingPrefix, format), v...)
 }
 
 func (l stdDebugLogger) InfoF(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("%s INFO: %s", loggingPrefix, format), v...)
 }
 
 func (l stdDebugLogger) DebugF(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("%s DEBUG: %s", loggingPrefix, format), v...)
 }
 
 func (l stdDebugLogger) TraceF(format string, v ...interface{}) {}
