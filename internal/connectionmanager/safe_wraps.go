4a0be05eac85ece8ecddcd6dc831388080d5dea2
magictucura <tucura@sub.red>
2024-12-16 09:12:20 +0100

    fix: goroutine leak in publisher on close
    - closing publisher did not close all associated goroutines. The
    routine listening for block events of connection only got terminated
    on closing the connection.
    The wrapping connection manager holds all blocking channels and only
    passes down an universal blocking channel now. If a signal reaches this
    channel it is broadcasted to all blocking channels. This allows telling
    the connection manager to remove and close the channel if publisher is
    closed.
    
    Closes #149

--- internal/connectionmanager/safe_wraps.go
+++ internal/connectionmanager/safe_wraps.go
@@ -8,10 +8,43 @@ import (
 func (connManager *ConnectionManager) NotifyBlockedSafe(
 	receiver chan amqp.Blocking,
 ) chan amqp.Blocking {
-	connManager.connectionMu.RLock()
-	defer connManager.connectionMu.RUnlock()
+	connManager.connectionMu.Lock()
+	defer connManager.connectionMu.Unlock()
 
-	return connManager.connection.NotifyBlocked(
-		receiver,
-	)
+	// add receiver to connection manager.
+	connManager.publisherNotifyBlockingReceiversMu.Lock()
+	connManager.publisherNotifyBlockingReceivers = append(connManager.publisherNotifyBlockingReceivers, receiver)
+	connManager.publisherNotifyBlockingReceiversMu.Unlock()
+
+	if !connManager.universalNotifyBlockingReceiverUsed {
+		connManager.connection.NotifyBlocked(
+			connManager.universalNotifyBlockingReceiver,
+		)
+		connManager.universalNotifyBlockingReceiverUsed = true
+	}
+
+	return receiver
+}
+
+// readUniversalBlockReceiver reads on universal blocking receiver and broadcasts event to all blocking receivers of
+// connection manager.
+func (connManager *ConnectionManager) readUniversalBlockReceiver() {
+	for b := range connManager.universalNotifyBlockingReceiver {
+		connManager.publisherNotifyBlockingReceiversMu.RLock()
+		for _, br := range connManager.publisherNotifyBlockingReceivers {
+			br <- b
+		}
+		connManager.publisherNotifyBlockingReceiversMu.RUnlock()
+	}
+}
+
+func (connManager *ConnectionManager) RemovePublisherBlockingReceiver(receiver chan amqp.Blocking) {
+	connManager.publisherNotifyBlockingReceiversMu.Lock()
+	for i, br := range connManager.publisherNotifyBlockingReceivers {
+		if br == receiver {
+			connManager.publisherNotifyBlockingReceivers = append(connManager.publisherNotifyBlockingReceivers[:i], connManager.publisherNotifyBlockingReceivers[i+1:]...)
+		}
+	}
+	connManager.publisherNotifyBlockingReceiversMu.Unlock()
+	close(receiver)
 }
