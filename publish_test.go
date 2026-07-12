88315724025f5b0076bad893221b90df16c65750
wagslane <lane.c.wagner@gmail.com>
2026-07-12 14:49:03 -0600

    fix: keep publisher alive when recovery startup fails

---
+++ publish_test.go
@@ -0,0 +1,32 @@
+package rabbitmq
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPublisherRestartContinuesAfterError(t *testing.T) {
+	reconnectErrCh := make(chan error, 2)
+	reconnectErrCh <- errors.New("first reconnect")
+	reconnectErrCh <- errors.New("second reconnect")
+	close(reconnectErrCh)
+
+	publisher := &Publisher{
+		reconnectErrCh: reconnectErrCh,
+		options: PublisherOptions{
+			Logger: simpleLogF(t.Logf),
+		},
+	}
+	restarts := 0
+	publisher.restartOnReconnect(func() error {
+		restarts++
+		if restarts == 1 {
+			return errors.New("broker failed during exchange declaration")
+		}
+		return nil
+	})
+
+	if restarts != 2 {
+		t.Fatalf("restart attempts = %d, want 2", restarts)
+	}
+}
